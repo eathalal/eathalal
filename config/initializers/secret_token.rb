f2898c353bdc17f09c2cafc7b483e781afaafe28
Eat Halal <eathalalltd@gmail.com>
2014-01-02 18:43:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eathalal::Application.config.secret_key_base = 'f329eda32dc652b494c8339ef2d02940d20bc01f2f9a1d9dbc9d5b61896149eaafc41a70115234f85f1c3cee61dd31972e8eabc413314cd9602383857e9ab94a'
