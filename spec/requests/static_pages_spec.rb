a6cd74e04cf9d78d0a5f5bc273805c6ddba85ffb
Eat Halal <eathalalltd@gmail.com>
2014-01-06 10:06:24 +0000

    Fixes failing specs.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,12 @@ describe "Static pages" do
   describe "Welcome page" do
 
 		it "should have the content 'Eat Halal" do
-		  visit '/static_pages/welcome'
+		  visit root_path
 		  expect(page).to have_content('Eat Halal')
 		end
 
 		it "should have the right title" do
-			visit '/static_pages/welcome'
+			visit root_path
 			expect(page).to have_title("Ahlan wa Sahlan | Eat Halal Ltd")
 		end
 
