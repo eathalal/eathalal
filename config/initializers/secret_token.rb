aa6734089b7e8bd099b4cb491862a09d37ac4320
Eat Halal <eathalalltd@gmail.com>
2014-01-06 07:53:51 +0000

    Replaces static secret token with dynamically generated secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Eathalal::Application.config.secret_key_base = 'f329eda32dc652b494c8339ef2d02940d20bc01f2f9a1d9dbc9d5b61896149eaafc41a70115234f85f1c3cee61dd31972e8eabc413314cd9602383857e9ab94a'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
