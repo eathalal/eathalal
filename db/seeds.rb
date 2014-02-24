6150e457a0646f881ad9c440a78a3b217f1d5cb6
Eat Halal <eathalalltd@gmail.com>
2014-02-24 15:12:07 +0000

    Adds category seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+categories = Category.create(
+		[
+			{ name: "Beef", description: "Beef is the culinary name for meat from bovines, especially cattle. Beef can be harvested from cows, bulls, heifers or steers." },
+			{ name: "Chicken", description: "Poultry; Meat from domesticated fowl" },
+			{ name: "Lamb", description: "The meat of a sheep in its first year." },
+			{ name: "Mutton", description: "The meat of an adult sheep." },
+			{ name: "Veal", description: "The meat of young cattle." },
+			{ name: "Burgers", description: "A round patty of ground meat." },
+			{ name: "Sausages", description: "Spiced ground meat in a natural casing." },
+			{ name: "Marinated Meats", description: "Meat covered in s seasoned liquid." }
+		]
+	)
