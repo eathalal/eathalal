d8ff618f4c31fcffe9061aa285bb0ac6d4484209
Eat Halal <eathalalltd@gmail.com>
2014-03-04 17:05:18 +0000

    Adds permissions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # callbacks
+  before_filter :authorise
+
+  delegate :allow?, to: :current_permission
+  helper_method :allow?
+
+  private
+  
+  def authorise
+    unless current_permission.allow?(params[:controller], params[:action], current_resource)
+      redirect_to root_url, alert: "Not authorised."
+    end
+  end
+
+  def current_permission
+    @current_permission ||= Permission.new(current_user)
+  end
+
+  def current_resource
+    nil
+  end
 end
